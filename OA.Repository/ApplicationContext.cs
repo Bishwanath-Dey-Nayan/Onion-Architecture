7284bd361b387f6c6bbd8e5c5e53ab4576f14a5d
GSA <Munirul_Islam@graycor.com>
2020-02-10 17:09:40 +0600

    Initial project structure

---
+++ OA.Repository/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OA.Repository
+{
+     public class ApplicationContext:DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
